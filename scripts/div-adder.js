e6ee93f5600f3c6366823ee3e0dc0b1f9cbc89d2
Noah Miles <noahemiles@gmail.com>
2021-02-20 11:45:40 -0800

    rename for clarity

---
+++ scripts/div-adder.js
@@ -0,0 +1,10 @@
+function divClick() {
+
+    let project_content = document.getElementById('project-content');
+
+    //for items in git
+    let newDiv = document.createElement('div');
+    newDiv.id = "project-panel";
+
+    project_content.appendChild(newDiv);
+}
