25d49d6d8c97f5e4af0694b03f61f23dd6d118fe
Noah Miles <noahemiles@gmail.com>
2021-02-20 15:30:09 -0800

    change to fetch and add git name and url to newly created divs

--- scripts/div-adder.js
+++ scripts/div-adder.js
@@ -1,10 +1,43 @@
+
+
+const getProjectUrls = new Promise((resolve, reject) => {
+    let urls = [];
+    try {
+        const response = fetch("https://api.github.com/users/noahemiles/repos")
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(repo => {
+                    urls.push([repo.name, repo.html_url]);
+                });
+            });
+        resolve(urls);
+    } catch (err) {
+        reject(new Error(`Error Fetching: ${err}`));
+    }
+
+});
+
+function test() {
+    getProjectUrls.then(data => {
+        console.log(data);
+    });
+}
+
 function divClick() {
 
     let project_content = document.getElementById('project-content');
 
-    //for items in git
-    let newDiv = document.createElement('div');
-    newDiv.id = "project-panel";
-
-    project_content.appendChild(newDiv);
+    //for repos in git    
+    getProjectUrls.then(data => {
+        data.forEach(nameUrl => {
+            let newDiv = document.createElement('div');
+            let newLink = document.createElement('a');
+            newDiv.id = "project-panel";
+            newDiv.innerHTML = nameUrl[0];
+            newLink.href = nameUrl[1];
+            newLink.appendChild(newDiv);
+            project_content.appendChild(newLink);
+        });
+    });
 }
+
