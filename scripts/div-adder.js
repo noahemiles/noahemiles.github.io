d7f3df4847be0e47bdce44aaf754b22046bee1c1
Noah Miles <noahemiles@gmail.com>
2021-02-20 18:14:59 -0800

    auto add git divs instead of when clicking div

--- scripts/div-adder.js
+++ scripts/div-adder.js
@@ -1,21 +1,21 @@
 
-
-const getProjectUrls = new Promise((resolve, reject) => {
-    let urls = [];
-    try {
-        const response = fetch("https://api.github.com/users/noahemiles/repos")
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(repo => {
-                    urls.push([repo.name, repo.html_url]);
+function getRepoUrls() {
+    return new Promise((resolve, reject) => {
+        let urls = [];
+        try {
+            fetch("https://api.github.com/users/noahemiles/repos")
+                .then(response => response.json())
+                .then(data => {
+                    data.forEach(repo => {
+                        urls.push([repo.name, repo.html_url]);
+                    });
+                    resolve(urls);
                 });
-            });
-        resolve(urls);
-    } catch (err) {
-        reject(new Error(`Error Fetching: ${err}`));
-    }
-
-});
+        } catch (err) {
+            reject(new Error(`Error Fetching: ${err}`));
+        }
+    });
+}
 
 function test() {
     getProjectUrls.then(data => {
@@ -23,23 +23,20 @@ function test() {
     });
 }
 
-function divClick() {
-
+function loadGitDivs(urls) {
     let project_content = document.getElementById('project-content');
-
-    //for repos in git    
-    getProjectUrls.then(data => {
-        data.forEach(nameUrl => {
-            let newDiv = document.createElement('div');
-            let newLink = document.createElement('a');
-            newDiv.id = "project-panel";
-            newDiv.innerHTML = nameUrl[0];
-            newLink.href = nameUrl[1];
-            newLink.appendChild(newDiv);
-            project_content.appendChild(newLink);
-        });
-    }).catch(err => {
-        console.log(`Error Adding Div: ${err}`);
+    //for repos in git
+    urls.forEach((nameUrl) => {
+        let newDiv = document.createElement('div');
+        let newLink = document.createElement('a');
+        newDiv.id = "project-panel";
+        newDiv.innerHTML = nameUrl[0];
+        newLink.href = nameUrl[1];
+        newLink.appendChild(newDiv);
+        project_content.appendChild(newLink);
     });
+
 }
 
+getRepoUrls()
+    .then(urls => loadGitDivs(urls));
