3bc6aa2cb0973db8e2e4d074d1ae3588bec20b64
Noah Miles <noahemiles@gmail.com>
2021-02-22 11:56:41 -0800

    nav bar fix

--- scripts/nav-function.js
+++ scripts/nav-function.js
@@ -15,3 +15,13 @@ function activeToggle(element) {
       }); 
     element.className += "active";
 }
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
