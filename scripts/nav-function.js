036a502ac90d889c24bd897bc51e136091239630
Noah Miles <noahemiles@gmail.com>
2021-02-22 12:45:02 -0800

    no more active toggle, responsive only

--- scripts/nav-function.js
+++ scripts/nav-function.js
@@ -7,21 +7,3 @@ function responsiveToggle() {
         x.className = "topnav";
     }
 }
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function activeToggle(element) {
-    var navItems = document.getElementById("myTopnav").children;
-    Array.from(navItems).forEach(function (navItem) { 
-        navItem.className = " ";
-      }); 
-    element.className += "active";
-}
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
