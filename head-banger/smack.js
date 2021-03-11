b3e8dff6ae6f8e8c23e9f8cdc01eb29e06994fa7
Noah Miles <noahemiles@gmail.com>
2021-03-10 22:16:58 -0800

    variable cleanup

--- head-banger/smack.js
+++ head-banger/smack.js
@@ -1,8 +1,8 @@
-document.getElementById("smacks_count").innerHTML = localStorage.getItem("smacks_count") || 0;
-document.getElementById("bangers_count").innerHTML = localStorage.getItem("bangers_count") || 0;
+document.getElementById("smack_count").innerHTML = localStorage.getItem("smack_count") || 0;
+document.getElementById("brick_count").innerHTML = localStorage.getItem("brick_count") || 0;
 function smack() {
-    var count = localStorage.getItem("smacks_count") || 0;
-    var counter = document.getElementById("smacks_count") ;
+    var count = localStorage.getItem("smack_count") || 0;
+    var counter = document.getElementById("smack_count") ;
     counter.innerHTML = count > 999 ? (count++).toExponential(count.toString().length - 1) : count++;
-    localStorage.setItem("smacks_count", count);
+    localStorage.setItem("smack_count", count);
 }
