61058082b32a9e738014735029cd42ebf0b9d31c
Noah Miles <noahemiles@gmail.com>
2021-03-10 20:38:52 -0800

    lower limit to use sci notif

--- head-banger/smack.js
+++ head-banger/smack.js
@@ -1,6 +1,6 @@
 function smack() {
     var count = localStorage.getItem("counter") || 0;
     var counter = document.getElementById("counter") ;
-    counter.innerHTML = count > 99999 ? (count++).toExponential(count.toString().length - 1) : count++;
+    counter.innerHTML = count > 999 ? (count++).toExponential(count.toString().length - 1) : count++;
     localStorage.setItem("counter", count);
 }
