1b1a9157345a6a69ff2977dd02b9ccfc66462e8b
Noah Miles <noahemiles@gmail.com>
2021-03-11 09:25:51 -0800

    legacy update for those who used original counter

--- head-banger/smack.js
+++ head-banger/smack.js
@@ -1,12 +1,17 @@
-var smack_count =
-  (Number(localStorage.getItem("counter")) >
-  Number(localStorage.getItem("smack_count"))
-    ? localStorage.getItem("counter")
-    : localStorage.getItem("smack_count")) || 0;
+var smack_count = Number(localStorage.getItem("smack_count")) || 0;
+//Legacy Update
+if (localStorage.getItem("counter")) {
+  smack_count += Number(localStorage.getItem("counter"));
+  localStorage.setItem("smack_count", smack_count);
+  localStorage.removeItem("counter");
+}
+
+//Initial set of Smack
 document.getElementById("smack_count").innerHTML =
   smack_count > 999
     ? Number(smack_count).toExponential(smack_count.toString().length - 1)
     : smack_count;
+//Initial set of Smack
 document.getElementById("brick_count").innerHTML =
   localStorage.getItem("brick_count") || 0;
 console.log(
