0e619bf99d715257d5a18ddd0e3d494d80d5effa
Noah Miles <noahemiles@gmail.com>
2021-03-11 08:58:21 -0800

    sci notif on load if > 999

--- head-banger/smack.js
+++ head-banger/smack.js
@@ -1,8 +1,24 @@
-document.getElementById("smack_count").innerHTML = localStorage.getItem("smack_count") || 0;
-document.getElementById("brick_count").innerHTML = localStorage.getItem("brick_count") || 0;
+var smack_count =
+  (Number(localStorage.getItem("counter")) >
+  Number(localStorage.getItem("smack_count"))
+    ? localStorage.getItem("counter")
+    : localStorage.getItem("smack_count")) || 0;
+document.getElementById("smack_count").innerHTML =
+  smack_count > 999
+    ? Number(smack_count).toExponential(smack_count.toString().length - 1)
+    : smack_count;
+document.getElementById("brick_count").innerHTML =
+  localStorage.getItem("brick_count") || 0;
+console.log(
+  Number(localStorage.getItem("counter")) >
+    Number(localStorage.getItem("smack_count"))
+);
 function smack() {
-    var count = localStorage.getItem("smack_count") || 0;
-    var counter = document.getElementById("smack_count") ;
-    counter.innerHTML = count > 999 ? (count++).toExponential(count.toString().length - 1) : count++;
-    localStorage.setItem("smack_count", count);
+  var count = localStorage.getItem("smack_count") || 0;
+  var smack_count = document.getElementById("smack_count");
+  smack_count.innerHTML =
+    count > 999
+      ? (count++).toExponential(count.toString().length - 1)
+      : count++;
+  localStorage.setItem("smack_count", count);
 }
