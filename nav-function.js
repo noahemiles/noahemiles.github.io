4c322aedea955b305a860a7466d3b2ad96acf50d
Noah Miles <noahemiles@gmail.com>
2021-08-04 22:45:25 -0700

    clean base

---
+++ nav-function.js
@@ -0,0 +1,22 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsiveToggle() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function addNavDivs() {
+  let nav = document.getElementById("myTopnav");
+  const links = ["index", "Projects", "Skills", "About"];
+  links.forEach((linkText) => {
+    let link = document.createElement("a");
+    link.innerHTML = linkText == "index" ? "Home" : linkText;
+    link.href = linkText + ".html";
+    nav.appendChild(link);
+  });
+}
+
+addNavDivs();
