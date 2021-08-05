e1be841c95edf17d0d32c196a98645ed4cd42789
Noah Miles <noahemiles@gmail.com>
2021-08-04 21:10:55 -0700

    handle days after christmas, but before new year

--- countdown/script.js
+++ countdown/script.js
@@ -6,11 +6,19 @@ const numbers = getHtmlElements();
 
 function setCounters() {
   const date = new Date();
-  numbers[0].innerHTML = 11 - date.getMonth();
-  numbers[1].innerHTML = 24 - date.getDay();
-  numbers[2].innerHTML = 24 - date.getHours();
-  numbers[3].innerHTML = 59 - date.getMinutes();
-  numbers[4].innerHTML = 60 - date.getSeconds();
+  if (date.getMonth() < 11 || date.getMonth() == 11 && date.getDay() < 25) {
+    numbers[0].innerHTML = 11 - date.getMonth(); //months are 0 based
+    numbers[1].innerHTML = 24 - date.getDay();
+    numbers[2].innerHTML = 23 - date.getHours();
+    numbers[3].innerHTML = 59 - date.getMinutes();
+    numbers[4].innerHTML = 60 - date.getSeconds();
+  } else {
+    numbers[0].innerHTML = 11;
+    numbers[1].innerHTML = 30 - date.getDay();
+    numbers[2].innerHTML = 23 - date.getHours();
+    numbers[3].innerHTML = 59 - date.getMinutes();
+    numbers[4].innerHTML = 60 - date.getSeconds();
+  }
 }
 
 setInterval(() => {
