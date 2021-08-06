b8e4ee609fe5af73df63846fb63beb7737f656b1
Noah Miles <noahemiles@gmail.com>
2021-08-05 18:21:22 -0700

    off by 1

--- countdown/script.js
+++ countdown/script.js
@@ -6,9 +6,9 @@ const numbers = getHtmlElements();
 
 function setCounters() {
   const date = new Date();
-  if (date.getMonth() < 11 || date.getMonth() == 11 && date.getDay() < 25) {
+  if (date.getMonth() < 11 || (date.getMonth() == 11 && date.getDay() < 25)) {
     numbers[0].innerHTML = 10 - date.getMonth(); //months are 0 based
-    numbers[1].innerHTML = 24 - date.getDay();
+    numbers[1].innerHTML = 23 - date.getDay();
     numbers[2].innerHTML = 23 - date.getHours();
     numbers[3].innerHTML = 59 - date.getMinutes();
     numbers[4].innerHTML = 60 - date.getSeconds();
