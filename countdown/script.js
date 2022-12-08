7c9b9c54c0ccc6135452440bbb119a066b1c174f
noahemiles <noahemiles@gmail.com>
2022-12-08 08:47:06 -0800

    handle months coming before xmas. after xmas and before new year still won't work

--- countdown/script.js
+++ countdown/script.js
@@ -6,19 +6,13 @@ const numbers = getHtmlElements();
 
 function setCounters() {
   const date = new Date();
-  if (date.getMonth() < 11 || (date.getMonth() == 11 && date.getDay() < 25)) {
-    numbers[0].innerHTML = 11 - date.getMonth(); //months are 0 based
-    numbers[1].innerHTML = 23 - date.getDay();
-    numbers[2].innerHTML = 23 - date.getHours();
-    numbers[3].innerHTML = 59 - date.getMinutes();
-    numbers[4].innerHTML = 60 - date.getSeconds();
-  } else {
-    numbers[0].innerHTML = 11;
-    numbers[1].innerHTML = 30 - date.getDay();
-    numbers[2].innerHTML = 23 - date.getHours();
-    numbers[3].innerHTML = 59 - date.getMinutes();
-    numbers[4].innerHTML = 60 - date.getSeconds();
-  }
+  let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  lastDay = lastDay.getDate();
+  numbers[0].innerHTML = 11 - date.getMonth(); //months are 0 based
+  numbers[1].innerHTML = (date.getMonth() < 11 & date.getDate() < 25 ? lastDay : 24) - date.getDate();
+  numbers[2].innerHTML = 23 - date.getHours();
+  numbers[3].innerHTML = 59 - date.getMinutes();
+  numbers[4].innerHTML = 60 - date.getSeconds();
 }
 
 setInterval(() => {
