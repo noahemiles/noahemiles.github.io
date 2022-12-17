f799a18f82c928ae44d9b8d5954d53bf39a2f13a
noahemiles <noahemiles@gmail.com>
2022-12-17 01:54:27 -0800

    solution

--- practice-exam/main.js
+++ practice-exam/main.js
@@ -1,28 +1,30 @@
 function app() {
     updateUser();
     let divs = document.querySelectorAll("div");
-    for (let i = 0; i < divs.length - 1; i++) {//=
+    for (let i = 0; i < divs.length - 1; i++) {
         divs[i].style.backgroundColor = turtleColors[divs[i].className];
     }
-    localStorage.setItem('seen', 'true');
 }
 
 const turtleColors = {
-    raphael: "red",
-    michelangelo: "orange",
     leonardo: "blue",
     donatello: "purple",
+    raphael: "red",
+    michelangelo: "orange",
     "master-splinter": "gray"
 };
 
 let updateUser = function () {
     let a = localStorage.getItem('seen');
     let b = [".raphael", ".leonardo", ".master-splinter"];
-    if (a === 'true') {
+    if (a) {
         b.forEach(badTurtle => {
             let t = document.querySelector(badTurtle);
             t.style.borderStyle = "dashed";
             t.style.borderColor = "white";
         });
+        localStorage.removeItem('seen');
+    } else {
+        localStorage.setItem('seen', 'true');
     }
 }
