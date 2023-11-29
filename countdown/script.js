a76cee2159712506f791d9b2c877221a73c28bd0
noahemiles <noahemiles@gmail.com>
2023-11-29 07:05:04 -0800

    fix countdown days

--- countdown/script.js
+++ countdown/script.js
@@ -7,12 +7,18 @@ const numbers = getHtmlElements();
 function setCounters() {
   const date = new Date();
   let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  let month = date.getMonth();
   lastDay = lastDay.getDate();
   numbers[0].innerHTML = 11 - date.getMonth(); //months are 0 based
-  numbers[1].innerHTML = (date.getMonth() < 11 & date.getDate() < 25 ? lastDay : 24) - date.getDate();
+  numbers[1].innerHTML = daysInThisMonth() - date.getDate();
   numbers[2].innerHTML = 23 - date.getHours();
   numbers[3].innerHTML = 59 - date.getMinutes();
   numbers[4].innerHTML = 60 - date.getSeconds();
+}
+
+function daysInThisMonth() {
+  var now = new Date();
+  return new Date(now.getFullYear(), now.getMonth()+1, 0).getDate();
 }
 
 setInterval(() => {
