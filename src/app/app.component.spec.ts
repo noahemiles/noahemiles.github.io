b63cdf68c8fe71bf586b55371b6fae8aeddc952d
Noah Miles <noahmiles@Noahs-MacBook-Pro.local>
2026-04-08 15:23:38 -0700

    add nav components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'myApp' title`, () => {
+  it(`should have the 'Noah Miles' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myApp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myApp');
+    expect(app.title).toEqual('Noah Miles');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'angular',
+    component: HomeComponent,
+    title: 'Home'
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Details - {{housingLocation?.name}}'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: "Contact"
+  }
+];
