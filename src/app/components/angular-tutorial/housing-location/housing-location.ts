357049e2545150b9249f8187bf7bd4ba9df78792
Noah Miles <noahemiles@gmail.com>
2026-06-05 10:09:58 -0700

    organize components within components folder

--- src/app/components/angular-tutorial/housing-location/housing-location.ts
+++ src/app/components/angular-tutorial/housing-location/housing-location.ts
@@ -1,5 +1,5 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
-import { HousingLocation } from '../housing-location';
+import { HousingLocation } from '../interfaces/housing-location';
 import { RouterLink } from '@angular/router';
 
 @Component({
