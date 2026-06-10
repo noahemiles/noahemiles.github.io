4323df844f7503c60752ad547961a6294b0de341
Noah Miles <noahemiles@gmail.com>
2026-06-10 13:37:27 -0700

    remove legacy error message test

--- e2e/form.spec.ts
+++ e2e/form.spec.ts
@@ -12,40 +12,38 @@ test.describe('Form', () => {
             const nameField = page.locator('#name');
             const emailField = page.locator('#email');
             const addUserButton = page.locator('button[type="submit"]');
-            const errorMessage = page.locator('.error-box');
             /* empty fields */
             await addUserButton.click();
-            await expect(errorMessage).toBeVisible();
+            await expect(page.locator('.error-box')).toBeVisible();
             
             /* No email */
             await nameField.fill('Test User');
             await emailField.fill('');
             await addUserButton.click();
-            await expect(errorMessage).toBeVisible();
+            await expect(page.locator('.error-box')).toBeVisible();
 
             /* No Name */
             await nameField.fill('');
             await emailField.fill('test@gmail.com');
             await addUserButton.click();
-            await expect(errorMessage).toBeVisible();
+            await expect(page.locator('.error-box')).toBeVisible();
             
             /* Invalid Email */
             await nameField.fill('');
             await emailField.fill('testgmail.com');
             await addUserButton.click();
-            await expect(errorMessage).toBeVisible();
+            await expect(page.locator('.error-box')).toBeVisible();
         });
         test('should add user when valid fields', async ({ page }) => {
             const nameField = page.locator('#name');
             const emailField = page.locator('#email');
             const addUserButton = page.locator('button[type="submit"]');
-            const errorMessage = page.locator('.error-box');
             
             await nameField.fill('Test User');
             await emailField.fill('test@gmail.com');
             
             await addUserButton.click();
-            await expect(errorMessage).toBeHidden();
+            await expect(page.locator('.error-box')).toBeHidden();
             await expect(page.locator('.user-tile')).toBeVisible();
             await expect(page.locator('.user-tile h2')).toHaveText('Test User');
             await expect(page.locator('.user-tile .user-content input.user-name')).toHaveValue('Test User');
@@ -55,7 +53,6 @@ test.describe('Form', () => {
             const nameField = page.locator('#name');
             const emailField = page.locator('#email');
             const addUserButton = page.locator('button[type="submit"]');
-            const errorMessage = page.locator('.error-box');
             
             await nameField.fill('Test User');
             await emailField.fill('test@gmail.com');
@@ -64,13 +61,12 @@ test.describe('Form', () => {
             const nameField = page.locator('#name');
             const emailField = page.locator('#email');
             const addUserButton = page.locator('button[type="submit"]');
-            const errorMessage = page.locator('.error-box');
             
             await nameField.fill('Test User');
             await emailField.fill('test@gmail.com');
             
             await addUserButton.click();
-            await expect(errorMessage).toBeHidden();
+            await expect(page.locator('.error-box')).toBeHidden();
             const userTile = page.locator('div.user-tile');
             await expect(userTile).toBeVisible();
 
