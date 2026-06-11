8f461177b690378b93687809a6153591659a7b6b
Noah Miles <noahemiles@gmail.com>
2026-06-11 09:48:20 -0700

    use icon component for projects

--- src/app/components/projects/projects-page/projects-page.ts
+++ src/app/components/projects/projects-page/projects-page.ts
@@ -1,13 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { ProjectTileComponent } from "../project-tile/project-tile";
+import { ProjectTileComponent } from '../project-tile/project-tile';
+import { BracketsIcon, GithubIcon } from '../../icons/icons/icons.component';
 
 @Component({
-    selector: 'app-projects-page',
-    imports: [ProjectTileComponent],
-    templateUrl: './projects-page.html',
-    changeDetection: ChangeDetectionStrategy.Eager,
-    styleUrl: './projects-page.css'
+  selector: 'app-projects-page',
+  imports: [ProjectTileComponent, BracketsIcon, GithubIcon],
+  templateUrl: './projects-page.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
+  styleUrl: './projects-page.css',
 })
-export class ProjectsPageComponent {
-
-}
+export class ProjectsPageComponent {}
