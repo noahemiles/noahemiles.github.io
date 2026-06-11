84ff4330a1e9a682b40c3c1eb047f03b87e0b897
noahemiles <noahemiles@gmail.com>
2026-06-11 09:50:17 -0700

    use icon component for projects (#25)

--- src/app/components/projects/project-tile/project-tile.ts
+++ src/app/components/projects/project-tile/project-tile.ts
@@ -1,13 +1,14 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ProjectDetails } from '../interfaces/project-details';
 
 @Component({
-    selector: 'app-project-tile',
-    imports: [RouterLink],
-    templateUrl: './project-tile.html',
-    changeDetection: ChangeDetectionStrategy.Eager,
-    styleUrl: './project-tile.css'
+  selector: 'app-project-tile',
+  imports: [RouterLink, CommonModule],
+  templateUrl: './project-tile.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
+  styleUrl: './project-tile.css',
 })
 export class ProjectTileComponent {
   @Input() projectDetails!: ProjectDetails;
